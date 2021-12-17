0cf7c0e6be5ba42b53dc065ee912e0b7346724a1
Walter Woshid <36635504+WalterWoshid@users.noreply.github.com>
2021-12-17 08:38:13 +0100

    Centered Vue logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img class="m-auto" alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
